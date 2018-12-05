4efeb78a4335f80047266513bb9c5066e93fc66e
Pactiy <45103658+Pactiy@users.noreply.github.com>
2018-12-05 19:26:48 +0200

    Add files via upload

---
+++ Calculator/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "1";
+            this.textBox1.Text = input;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "2";
+            this.textBox1.Text = input;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "3";
+            this.textBox1.Text = input;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "4";
+            this.textBox1.Text = input;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "5";
+            this.textBox1.Text = input;
+        }
+       
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "6";
+            this.textBox1.Text = input;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "7";
+            this.textBox1.Text = input;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "8";
+            this.textBox1.Text = input;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "9";
+            this.textBox1.Text = input;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "0";
+            this.textBox1.Text = input;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            BackColor = Color.White;
+            input = null;
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            long x;
+            int i, ok = 0;
+
+            try
+            {
+                x = Convert.ToInt64(input);
+            }
+
+            catch (Exception)
+            {
+                x = 0;
+            }
+            if(x>999999)
+            { x = 0; }
+            if (x == 1 || x == 0)
+            { ok = 1; }
+            for (i = 2; i <= x / 2; i++)
+            { if (x % i == 0)
+                    ok = 1;
+            }
+            if (ok == 0)
+            {
+                this.textBox1.Text = "";
+                BackColor = System.Drawing.Color.Green;
+            }
+            else
+            { this.textBox1.Text = "";
+              BackColor = System.Drawing.Color.Red;
+            }
+            input = null;
+
+
+
+
+
+        }
+    }
+}
